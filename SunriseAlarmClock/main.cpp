7e2a04474cc4f816be138d8752d477f1c7a3a049
Matthew Sainsbury <matthew@sainsbury.za.net>
2016-10-09 18:25:06 +0200

    Move time-fetching to RTC-based interrupt

--- SunriseAlarmClock/main.cpp
+++ SunriseAlarmClock/main.cpp
@@ -1,6 +1,7 @@
 #include "config.h"
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #include <util/delay.h>
 
 #include "sevensegment.h"
@@ -46,15 +47,27 @@ void error() {
     ledOn();
     ledOff();
 }
+
+// Pointer to the latest time fetched from the RTC
+Time* volatile time;
+// Swapping variables for setting time pointer.
+Time t1, t2;
+
 int main(void){
     I2C::enable();
     SevenSegment::enable();
-    
+
+    // Enable 1Hz signal from RTC and enable interrupt routine to set the time
+    RTC::writeRegister(0xe, 0); //enable 1Hz signal from RTC
+
+    EICRA = 0b1100; // enable interrupt on rising edge of 1Hz signal
+    EIMSK = 0b10;
+    sei();
+
+    while (time == 0) ; //wait for sync
 
     for(;;){
-        ledOn();
-        Time t = RTC::getTime();
-        ledOff();
+        Time t = *time;
         SevenSegment::digits[0] = SevenSegment::encodeDigit(t.hour/10);
         SevenSegment::digits[1] = SevenSegment::encodeDigit(t.hour%10);
         SevenSegment::digits[2] = SevenSegment::encodeDigit(t.minute/10);
@@ -63,5 +76,16 @@ int main(void){
             SevenSegment::points = 1<<2 | 1<<3;
         else SevenSegment::points = 0;
         SevenSegment::show();
+    }
+}
+
+ISR(INT1_vect) {
+    Time t = RTC::getTime();
+    if (time == &t1) {
+        t2 = t;
+        time = &t2;
+    } else {
+        t1 = t;
+        time = &t1;
     }
 }
